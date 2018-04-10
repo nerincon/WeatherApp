e26ab3999196c0d42990149d0893e0a96923ec31
nerincon <nerincon1@gmail.com>
2018-04-09 23:23:56 -0500

    add db, create schema, start API cache

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import tornado.ioloop
 import tornado.web
 import tornado.log
-
+import psycopg2
+import datetime
 
 import json
 import requests
@@ -15,6 +16,7 @@ ENV = Environment(
   loader=PackageLoader('myapp', 'templates'),
   autoescape=select_autoescape(['html', 'xml'])
 )
+
 
 class TemplateHandler(tornado.web.RequestHandler):
   def render_template (self, tpl, context):
@@ -31,6 +33,19 @@ class MainHandler(TemplateHandler):
       payload = {'q': city, 'APPID':'02485176f4257a69383d168ccf8c169c', 'units':'imperial'}
       r = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
       weather_data = r.json()
+      temp = weather_data['main']['temp']
+      temp_min = weather_data['main']['temp_min']
+      temp_max = weather_data['main']['temp_max']
+      description = weather_data['weather'][0]['description']
+      wind_speed = weather_data['wind']['speed']
+      nextTime = datetime.datetime.now() + datetime.timedelta(minutes = 15)
+      conn = psycopg2.connect("dbname=weather_db user=postgres")
+      cur = conn.cursor()
+      dt = datetime.datetime.now()
+      cur.execute("INSERT INTO info VALUES (%s, %s, %s, %s, %s, %s, %s)",(city, temp, temp_min, temp_max, description, wind_speed, dt))
+      conn.commit()
+      cur.close()
+      conn.close()
     self.set_header(
       'Cache-Control',
       'no-store, no-cache, must-revalidate, max-age=0')
@@ -48,5 +63,5 @@ def make_app():
 if __name__ == "__main__":
   tornado.log.enable_pretty_logging()
   app = make_app()
-  app.listen(int(os.environ.get('PORT', '8080')))
+  app.listen(int(os.environ.get('PORT', '8000')))
   tornado.ioloop.IOLoop.current().start()
---
+++ weather_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE info (
+  city VARCHAR,
+  temp VARCHAR,
+  temp_min VARCHAR,
+  temp_max VARCHAR,
+  description VARCHAR,
+  wind_speed VARCHAR,
+  time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
